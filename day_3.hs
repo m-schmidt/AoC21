396a2cc099ed5a6396e1c6486554d21634eb8d74
Michael Schmidt <github@mschmidt.me>
2021-12-03 20:31:58 +0100

    day 3

---
+++ day_3.hs
@@ -0,0 +1,57 @@
+-- https://adventofcode.com/2021/day/3
+
+import Data.List
+import Data.Maybe
+
+
+-- 1 Gamme/epsilon rate
+parseBitString :: String -> [Int]
+parseBitString = mapMaybe parse
+    where
+        parse '1' = Just 1
+        parse '0' = Just 0
+        parse  _  = Nothing
+
+toInt :: [Int] -> Int
+toInt = foldl (\a b -> a*2 + b) 0
+
+-- threshold for most/least bits - roundes upwards in case number of input lines is odd
+threshold :: [a] -> Int
+threshold xs = (length xs + 1) `div` 2
+
+count :: Int -> [Int] -> Int
+count t bs = if sum bs > t then 1 else 0
+
+
+-- 2 Life support rating
+count' :: (Int -> Int -> Bool) -> [[Int]] -> [Int]
+count' cmp [b] = b
+count' cmp bss =
+    let (bss0, bss1) = partition ([0] `isPrefixOf`) bss in
+    let (bit, bss')  = if length bss0 `cmp` length bss1 then (1, bss1) else (0, bss0) in
+    bit : count' cmp (map tail bss')
+
+-- Oxygen: select 1, when there are fewer 0s than 1s
+countOxygen :: [[Int]] -> [Int]
+countOxygen = count' (<=)
+
+-- CO2: select 1, when there are more 0s than 1s
+countCO2 :: [[Int]] -> [Int]
+countCO2 = count' (>)
+
+
+main :: IO ()
+main = do
+    bits <- map parseBitString . lines <$> readFile "day_3_input.txt"
+    let bits' = transpose bits
+
+    -- since no filtering is needed count on transposed bit strings
+    let most    = count (threshold bits) <$> bits'
+    let gamma   = toInt most
+    let epsilon = toInt $ map (1-) most
+    putStrLn $ "#1: " ++ show (gamma * epsilon)
+
+    -- count with filtering
+    let oxygen =  toInt $ countOxygen bits
+    let co2    =  toInt $ countCO2 bits
+    putStrLn $ "#2: " ++ show (oxygen * co2)
