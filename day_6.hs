ce08966b57e96234adb218463987b4a7f5b01536
Michael Schmidt <github@mschmidt.me>
2021-12-06 13:47:27 +0100

    day 6

--- day_6.hs
+++ day_6.hs
@@ -2,24 +2,22 @@
 
 import qualified Data.Map.Strict as M
 
-
-initAges :: [Int] -> M.Map Int Int
-initAges = foldr (M.alter f) emptyAges
+toMap :: [Int] -> M.Map Int Int
+toMap = foldr (M.alter f) emptyAges
     where
         emptyAges  = M.fromList $ zip [0..8] (repeat 0)
         f (Just n) = Just (n+1)
         f Nothing  = undefined
 
-updateAges :: M.Map Int Int -> M.Map Int Int
-updateAges ages = M.mapWithKey f ages
+simulate :: M.Map Int Int -> M.Map Int Int
+simulate ages = M.mapWithKey f ages
     where
-        get k = M.findWithDefault 0 k ages
-        f 6 _ = get 0 + get 7
-        f 8 _ = get 0 + get 9
-        f n _ = get (n+1)
+        f 6 _ = ages M.! 0 + ages M.! 7
+        f 8 _ = ages M.! 0
+        f n _ = ages M.! (n+1)
 
 count :: Int -> M.Map Int Int -> Int
-count n ages = sum . M.elems $ iterate updateAges ages !! n
+count n ages = sum . M.elems $ iterate simulate ages !! n
 
 parse :: String -> [Int]
 parse = map read . words . map go
@@ -27,6 +25,6 @@ parse = map read . words . map go
 
 main :: IO ()
 main = do
-    ages <- initAges. parse <$> readFile  "day_6_input.txt"
+    ages <- toMap . parse <$> readFile  "day_6_input.txt"
     putStrLn $ "#1: " ++ show (count 80 ages)
     putStrLn $ "#2: " ++ show (count 256 ages)
